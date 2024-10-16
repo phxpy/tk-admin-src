ef13e2b0bd8bd4ae422e20d03f35a7eabd224079
phxpy <phxpy@protonmail.com>
2024-10-16 17:52:57 +0500

    fix base

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,6 @@ import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/tk-admin/',
   plugins: [
     // Docs: https://github.com/posva/unplugin-vue-router
     // ℹ️ This plugin should be placed before vue plugin
